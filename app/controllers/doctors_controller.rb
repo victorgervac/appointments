1aa88b1565edb258c1ce37b7ed2a0305b7f0d197
Victor Gervacio <victorgervacio33@gmail.com>
2020-08-29 21:17:20 -0600

    first commit

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,10 @@
+class DoctorsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
---
+++ test/controllers/doctors_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class DoctorsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get doctors_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get doctors_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get doctors_edit_url
+    assert_response :success
+  end
+
+end
